60f8e49b424e7e8e13b874fe46ad6b8f025ecda1
Richard Rast <richard.rast@trinetx.com>
2023-12-02 19:32:29 -0500

    chore: update autogen files

--- src/day22.rs
+++ src/day22.rs
@@ -1,4 +1,4 @@
-const INPUT_FILE: &'static str = "input/01.txt";
+const INPUT_FILE: &'static str = "input/22.txt";
 
 pub fn a() -> String {
     let input = std::fs::read_to_string(INPUT_FILE).expect("Input should exist");
