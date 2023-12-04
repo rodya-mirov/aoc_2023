66ee6da7800ac0947b045acf2e67c7590d7ec09a
Richard Rast <richard.rast@trinetx.com>
2023-12-03 22:00:01 -0500

    feat: day 3

--- src/day02.rs
+++ src/day02.rs
@@ -14,15 +14,21 @@ pub fn a() -> String {
 }
 
 fn a_with_input(input: &str) -> usize {
-    input.lines().map(a_one_game).filter(|(_id, is_valid)| *is_valid).map(|(id, _)| id).sum()
+    input
+        .lines()
+        .map(a_one_game)
+        .filter(|(_id, is_valid)| *is_valid)
+        .map(|(id, _)| id)
+        .sum()
 }
 
 /// Returns (id, is_valid) for the parsed game
 fn a_one_game(input: &str) -> (usize, bool) {
     let game_record = parse_game(input);
-    let is_valid = game_record.pulls.iter().all(|p| {
-        p.num_red <= 12 && p.num_green <= 13 && p.num_blue <= 14
-    });
+    let is_valid = game_record
+        .pulls
+        .iter()
+        .all(|p| p.num_red <= 12 && p.num_green <= 13 && p.num_blue <= 14);
     (game_record.id, is_valid)
 }
 
@@ -172,11 +178,31 @@ mod tests {
     #[test]
     fn a_samples() {
         let fixtures = [
-            ("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", 1, true),
-            ("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", 2, true),
-            ("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", 3, false),
-            ("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red", 4, false),
-            ("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", 5, true)
+            (
+                "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+                1,
+                true,
+            ),
+            (
+                "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+                2,
+                true,
+            ),
+            (
+                "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+                3,
+                false,
+            ),
+            (
+                "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+                4,
+                false,
+            ),
+            (
+                "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+                5,
+                true,
+            ),
         ];
 
         for (input, id, valid) in fixtures {
@@ -193,10 +219,19 @@ mod tests {
     fn b_samples() {
         let fixtures = [
             ("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", 48),
-            ("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", 12),
-            ("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", 1560),
-            ("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red", 630),
-            ("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", 36)
+            (
+                "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+                12,
+            ),
+            (
+                "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+                1560,
+            ),
+            (
+                "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+                630,
+            ),
+            ("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", 36),
         ];
 
         for (input, exp) in fixtures {
--- src/day03.rs
+++ src/day03.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 const INPUT_FILE: &'static str = "input/03.txt";
 
 pub fn a() -> String {
@@ -5,8 +7,62 @@ pub fn a() -> String {
     a_with_input(&input).to_string()
 }
 
-fn a_with_input(_input: &str) -> String {
-    unimplemented!()
+fn a_with_input(input: &str) -> u64 {
+    // this is the worst thing i've ever written, maybe :thinking:
+    let adj_locations: HashSet<(usize, usize)> = input
+        // go through each line and collect all the coordinates of special characters ...
+        .lines()
+        .enumerate()
+        .map(|(row_ind, line)| {
+            line.chars()
+                .enumerate()
+                .filter(|&(_, c)| !c.is_alphanumeric() && !(c == '.'))
+                .map(move |(col_ind, _)| (row_ind as i32, col_ind as i32))
+        })
+        .flatten()
+        // ... then for each coordinate of same, emit every coordinate adjacent to any of those ...
+        .map(|(row, col)| {
+            (-1..=1)
+                .map(move |dx| (-1..=1).map(move |dy| (row + dy, col + dx)))
+                .flatten()
+        })
+        .flatten()
+        .filter(|&(row, col)| row >= 0 && col >= 0)
+        .map(|(row, col)| (row as usize, col as usize))
+        // ... and collection the result as a hashset
+        .collect();
+
+    let mut total_adj = 0;
+
+    for (row_ind, line) in input.lines().enumerate() {
+        let mut is_running = false;
+        let mut is_adj = false;
+        let mut running_total = 0;
+
+        for (col_ind, c) in line.chars().enumerate() {
+            if c.is_numeric() {
+                if adj_locations.contains(&(row_ind, col_ind)) {
+                    is_adj = true;
+                }
+                is_running = true;
+                running_total = running_total * 10 + ((c as u64) - ('0' as u64));
+            } else {
+                if is_running && is_adj {
+                    total_adj += running_total;
+                }
+
+                is_running = false;
+                is_adj = false;
+                running_total = 0;
+            }
+        }
+
+        if is_running && is_adj {
+            total_adj += running_total;
+        }
+    }
+
+    total_adj
 }
 
 pub fn b() -> String {
@@ -14,16 +70,112 @@ pub fn b() -> String {
     b_with_input(&input).to_string()
 }
 
-fn b_with_input(_input: &str) -> String {
-    unimplemented!()
+fn b_with_input(input: &str) -> u64 {
+    // this is a little less bad than the previous
+    let gear_adjacency: Vec<HashSet<(usize, usize)>> = input
+        // go through each line and collect all the coordinates of gears ...
+        .lines()
+        .enumerate()
+        .map(|(row_ind, line)| {
+            line.chars()
+                .enumerate()
+                .filter(|&(_, c)| c == '*')
+                .map(move |(col_ind, _)| (row_ind as i32, col_ind as i32))
+        })
+        .flatten()
+        // ... then for each coordinate of same, emit every coordinate adjacent to any of those ...
+        .map(|(row, col)| {
+            (-1..=1)
+                .map(move |dx| (-1..=1).map(move |dy| (row + dy, col + dx)))
+                .flatten()
+                .filter(|&(r, c)| r >= 0 && c >= 0)
+                .map(|(r, c)| (r as usize, c as usize))
+                .collect::<HashSet<_>>()
+        })
+        // then just blob them up into a list of sets
+        .collect();
+
+    let mut gear_parts: Vec<Vec<u64>> = vec![Vec::new(); gear_adjacency.len()];
+
+    for (row_ind, line) in input.lines().enumerate() {
+        let mut is_running = false;
+        let mut adj_gears: HashSet<usize> = HashSet::new();
+        let mut running_total = 0;
+
+        for (col_ind, c) in line.chars().enumerate() {
+            if c.is_numeric() {
+                for (gear_ind, adj_set) in gear_adjacency.iter().enumerate() {
+                    if adj_set.contains(&(row_ind, col_ind)) {
+                        adj_gears.insert(gear_ind);
+                    }
+                }
+
+                is_running = true;
+                running_total = running_total * 10 + ((c as u64) - ('0' as u64));
+            } else {
+                if is_running {
+                    for gear_ind in adj_gears.iter().copied() {
+                        gear_parts[gear_ind].push(running_total);
+                    }
+                }
+
+                is_running = false;
+                adj_gears.clear();
+                running_total = 0;
+            }
+        }
+
+        if is_running {
+            for gear_ind in adj_gears.iter().copied() {
+                gear_parts[gear_ind].push(running_total);
+            }
+        }
+    }
+
+    // finally, add up the derived power of all the gears
+    let mut actual_total = 0;
+
+    for gear_part in gear_parts.into_iter() {
+        if gear_part.len() == 2 {
+            actual_total += gear_part[0] * gear_part[1];
+        }
+    }
+
+    actual_total
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    const SAMPLE_A: &'static str = "467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..";
+
     #[test]
-    fn hello_world() {
-        assert_eq!(2 + 2, 4)
+    fn sample_a_test() {
+        assert_eq!(a_with_input(SAMPLE_A), 4361);
+    }
+
+    #[test]
+    fn sample_a_test2() {
+        let input = "
+467..114
+...*..!.
+..35..63"
+            .trim();
+        assert_eq!(a_with_input(input), 467 + 114 + 35 + 63);
+    }
+
+    #[test]
+    fn sample_b_test() {
+        assert_eq!(b_with_input(SAMPLE_A), 467 * 35 + 755 * 598);
     }
 }
